6b6a50f1b0781221035385ebf45257990049835a
Пламен Желязков Проданов <dabidesh@gmail.com>
2019-12-07 16:14:19 +0200

    Първонач. 2 цъквания вече не са необходими

--- js/kr-lod.js
+++ js/kr-lod.js
@@ -15,8 +15,10 @@ window.onload=function() {
 
 function pokKrij() {
     elKr= getElementById0('nav-ul');
-    //alert(elKr.style.display);
-    if (elKr.style.display == "block") {
+    style = window.getComputedStyle(elKr);
+    display = style.getPropertyValue('display');
+    //alert(display);
+    if (display == "block") {
       elKr.style.display= 'none';
       document.getElementById("navid").style.height = "4em";
     }
@@ -27,7 +29,9 @@ function pokKrij() {
 }
 function pokKrij0() {
     elKr= getElementById0('oshte-div');
-    if (elKr.style.display == "block") {
+    style = window.getComputedStyle(elKr);
+    display = style.getPropertyValue('display');
+    if (display == "block") {
       elKr.style.display= 'none';
       document.getElementById("asideid").style.height = "4em";
     }
@@ -43,7 +47,10 @@ function copyid(id) {
   document.execCommand("copy");
 }
 function smenipos(id1, id2) {
-  if (id1.style.display == 'block') {
+  style = window.getComputedStyle(id1);
+  display = style.getPropertyValue('display');
+  //alert(id2.style.display);
+  if (display == 'block') {
     id1.style.display= 'none';
     id2.style.display= 'block';
   }
