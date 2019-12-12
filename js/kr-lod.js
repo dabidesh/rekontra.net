f54edf2a15697369ec4e07e31dba0f31ebab4c53
Пламен Желязков Проданов <dabidesh@gmail.com>
2019-12-12 15:50:31 +0200

    без „;“

--- js/kr-lod.js
+++ js/kr-lod.js
@@ -1,43 +1,43 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementsByTagName('body')[0].classList.add('loaded');
-});
+    document.getElementsByTagName('body')[0].classList.add('loaded')
+})
 
 // оти не работи ? 
-var element, elKr;
+var element, elKr
 //var flag=false;
 window.onload=function() {
-  element= getElementById0('nav');
-  addEvent('click', element, pokKrij);
+  element= getElementById0('nav')
+  addEvent('click', element, pokKrij)
   
-  element= getElementById0('olex');
-  addEvent('click', element, pokKrij0);
+  element= getElementById0('olex')
+  addEvent('click', element, pokKrij0)
 }
 
 function pokKrij() {
-    elKr= getElementById0('nav-ul');
-    style = window.getComputedStyle(elKr);
-    display = style.getPropertyValue('display');
-    //alert(display);
+    elKr= getElementById0('nav-ul')
+    style = window.getComputedStyle(elKr)
+    display = style.getPropertyValue('display')
+    //alert(display)
     if (display == "block") {
-      elKr.style.display= 'none';
-      document.getElementById("navid").style.height = "4em";
+      elKr.style.display= 'none'
+      document.getElementById("navid").style.height = "4em"
     }
     else {
       elKr.style.display= 'block';
-      document.getElementById("navid").style.height = "auto";
+      document.getElementById("navid").style.height = "auto"
     }
 }
 function pokKrij0() {
-    elKr= getElementById0('oshte-div');
-    style = window.getComputedStyle(elKr);
-    display = style.getPropertyValue('display');
+    elKr= getElementById0('oshte-div')
+    style = window.getComputedStyle(elKr)
+    display = style.getPropertyValue('display')
     if (display == "block") {
-      elKr.style.display= 'none';
-      document.getElementById("asideid").style.height = "4em";
+      elKr.style.display= 'none'
+      document.getElementById("asideid").style.height = "4em"
     }
     else {
       elKr.style.display= 'block';
-      document.getElementById("asideid").style.height = "auto";
+      document.getElementById("asideid").style.height = "auto"
     }
 }
 
@@ -59,20 +59,20 @@ function smenipos(id1, id2) {
     id2.style.display= 'none'
   }
 }
-
+// стари, да се премахне поддръжката за Експлодъра
 function getElementById0(name) {
 var o= document.getElementById
 ? document.getElementById(name) : document.all
-? document.all[name] : document.layers[name];
-return o;
+? document.all[name] : document.layers[name]
+return o
 }
 function addEvent(event, elem, func) {
    if (elem.addEventListener)  // W3C DOM
-      elem.addEventListener(event,func,false);
+      elem.addEventListener(event,func,false)
    else if (elem.attachEvent) { // Експлодър DOM
-      elem.attachEvent("on"+event, func);
+      elem.attachEvent("on"+event, func)
    }
    else {
-      elem[event] = func;
+      elem[event] = func
    }
 }
